8a39dc8b52babd8e1d3658f68bbe88613b31f147
pvalderrama <pvalderrama@asesoftware.com>
2021-06-20 15:44:31 -0500

    creacion loggers en controller y service de comercio

--- src/main/java/com/asesoftware/semilla/turnos/controller/ComercioController.java
+++ src/main/java/com/asesoftware/semilla/turnos/controller/ComercioController.java
@@ -19,7 +19,7 @@ import com.asesoftware.semilla.turnos.service.IComercioService;
 @RequestMapping(path = "/api/v1/comercio")
 public class ComercioController {
 	
-	//private static final Logger logger = LoggerFactory.getLogger(ComercioController.class);
+	private static final Logger logger = LoggerFactory.getLogger(ComercioController.class);
 	
 	@Autowired
 	private IComercioService comercioService;
@@ -27,18 +27,32 @@ public class ComercioController {
 	//listar todos	
 	@GetMapping(path = "/all")
 	public ResponseDTO getAll(){
+		
+		logger.info("ingreso al metodo getAll ");
+		
 		return comercioService.getAll();
 	}
 	
 	//listar comercio por id
 	@GetMapping(path = "/comercio/{id}")
 	public ResponseDTO getComercioById(@PathVariable Integer id) {
+		
+		logger.info("ingreso al metodo getComercioById ");
+
+		logger.info("comercio a consultar con el id {}", id);
+		
+		
 		return comercioService.getComercioById(id);
 	}
 	
 	//mostrar por parametro id	
 	@GetMapping(path = "/comercio")
 	public ResponseDTO getComercioParametroById(@RequestParam Integer id) {
+		
+		logger.info("ingreso al metodo getComercioParametroById ");
+
+		logger.info("comercio a consultar con el id {}", id);
+		
 		return comercioService.getComercioById(id);
 	}
 	
@@ -46,18 +60,33 @@ public class ComercioController {
 	//crear comercio
 	@PostMapping(path = "/crear", consumes = "application/json", produces = "application/json")
 	public ResponseDTO createComercio(@RequestBody ComerciosDTO entity) {
+		
+		logger.info("ingreso al metodo createComercio ");
+
+		logger.info("comercio a crear con los datos {}", entity);
+		
 		return comercioService.createComercio(entity);
 	}
 	
 	//editar comercio
 	@PostMapping(path = "/editar", consumes = "application/json", produces = "application/json")
 	public ResponseDTO updateComercio(@RequestBody ComerciosDTO entity) {
+		
+		logger.info("ingreso al metodo updateComercio ");
+
+		logger.info("comercio a actualizar con los datos {}", entity);
+		
 		return comercioService.updateComercio(entity);
 	} 
 	
 	//eliminar comercio
 	@GetMapping(path = "/delete/{id}")
 	public ResponseDTO deleteComercio(@PathVariable Integer id) {
+		
+		logger.info("ingreso al metodo deleteComercio ");
+
+		logger.info("comercio a eliminar {}",id);
+		
 		return comercioService.deleteComercio(id);
 	}
 	
--- src/main/java/com/asesoftware/semilla/turnos/service/ComercioService.java
+++ src/main/java/com/asesoftware/semilla/turnos/service/ComercioService.java
@@ -2,10 +2,11 @@ package com.asesoftware.semilla.turnos.service;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
 import com.asesoftware.semilla.turnos.dto.ComerciosDTO;
 import com.asesoftware.semilla.turnos.dto.ResponseDTO;
 import com.asesoftware.semilla.turnos.entity.ComercioEntity;
@@ -15,6 +16,8 @@ import com.asesoftware.semilla.turnos.repository.IComercioRepository;
 @Service
 public class ComercioService implements IComercioService{
 	
+	private static final Logger logger  = LoggerFactory.getLogger(ComercioService.class);
+	
 	@Autowired
 	private IComercioRepository comercioRepository;
 	
@@ -23,16 +26,27 @@ public class ComercioService implements IComercioService{
 	
 	@Override
 	public ResponseDTO getAll() {
+		
+		logger.info("ingreso al metodo getAll ");
+		
+		logger.info("reresa todos los comercios ");
+		
 		return new ResponseDTO(mapperComercio.listEntityToDto(comercioRepository.findAll()), true, "ok", HttpStatus.OK); 
 	}
 	
 	@Override
 	public ResponseDTO getComercioById(Integer id) {
+		
+		logger.info("ingreso al metodo getComerciobyId");
+		
 		Optional<ComercioEntity> optional = comercioRepository.findById(id);
 		
 		if(optional.isPresent()) {
+			logger.info("comercio encontrado");
 			return new ResponseDTO(optional.get(), true, "ok", HttpStatus.OK);
 		}else {
+			
+			logger.info("no se encontro el comercio");			
 			return new ResponseDTO(null, false, "comercio no encontrado", HttpStatus.OK);
 		}
 	}
@@ -41,11 +55,21 @@ public class ComercioService implements IComercioService{
 	
 	@Override
 	public ResponseDTO createComercio(ComerciosDTO comerciosDTO) {
+		
+		logger.info("ingreso al metodo CreateComercio");
+		
 		try {			
 			ComercioEntity comercioEntity = mapperComercio.dtoToEntity(comerciosDTO);
+			
 			comercioRepository.save(comercioEntity);
+			
+			logger.info("se creo el comercio");
+			
 			return new ResponseDTO(mapperComercio.entityToDto(comercioEntity), true, "comercio creado correctamente", HttpStatus.OK);
 		}catch(Exception e) {
+			
+			logger.error("Error {}",e.getMessage());
+			
 			return new ResponseDTO(null, false, "el comercio no puede ser creado", HttpStatus.OK);
 		}
 		
@@ -54,9 +78,13 @@ public class ComercioService implements IComercioService{
 	@Override
 	public ResponseDTO updateComercio(ComerciosDTO comerciosDTO) {
 		
+		logger.info("ingreso al metodo updateComercio");
+		
 		ComercioEntity comercioEntity = mapperComercio.dtoToEntity(comerciosDTO);
 		
 		comercioRepository.save(comercioEntity);
+		
+		logger.info("comercio actualizado");
 		
 		return new ResponseDTO(mapperComercio.entityToDto(comercioEntity), true, "comercio actualizado correctamente", HttpStatus.OK);
 	}
@@ -65,10 +93,17 @@ public class ComercioService implements IComercioService{
 	@Override
 	public ResponseDTO deleteComercio(Integer id) {
 		
+		logger.info("ingreso al metodo deleteComercio");
+		
 		try {
 			comercioRepository.deleteById(id);
+			
+			logger.info("el comercio {} se elimino",id);
+			
 			return new ResponseDTO(null, true, "comercio eliminado", HttpStatus.OK);
 		}catch (Exception e) {
+			logger.error("Error {}",e.getMessage());
+			
 			return new ResponseDTO(null, false, "comercio no existe", HttpStatus.OK);
 		}
 		
